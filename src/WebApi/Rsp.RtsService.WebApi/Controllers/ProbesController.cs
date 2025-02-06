40be5e9395336113b0e6c5542c696ef70133a6c6
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-02-06 15:50:07 +0000

    enable logging to probes controller

--- src/WebApi/Rsp.RtsService.WebApi/Controllers/ProbesController.cs
+++ src/WebApi/Rsp.RtsService.WebApi/Controllers/ProbesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Rsp.Logging.Extensions;
 
 namespace Rsp.RtsService.WebApi.Controllers;
 
@@ -6,21 +8,57 @@ namespace Rsp.RtsService.WebApi.Controllers;
 [Route("[controller]")]
 public class ProbesController : ControllerBase
 {
+    private ILogger<ProbesController> _logger;
+
+    public ProbesController(ILogger<ProbesController> logger)
+    {
+        _logger = logger;
+    }
     [HttpGet("liveness")]
     public IActionResult Liveness()
     {
-        return Ok();
+        try
+        {
+            _logger.LogAsInformation("Sucesfully called the LIVENESS probe.");
+            return Ok();
+;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogAsError("SERVER_ERROROR", ex.Message, ex);
+            throw;
+        }
     }
 
     [HttpGet("readiness")]
     public IActionResult Readiness()
     {
-        return Ok();
+        try
+        {
+            _logger.LogAsInformation("Sucesfully called the READINESS probe.");
+            return Ok();
+            ;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogAsError("SERVER_ERROROR", ex.Message, ex);
+            throw;
+        }
     }
 
     [HttpGet("startup")]
     public IActionResult Startup()
     {
-        return Ok();
+        try
+        {
+            _logger.LogAsInformation("Sucesfully called the STARTUP probe.");
+            return Ok();
+            ;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogAsError("SERVER_ERROROR", ex.Message, ex);
+            throw;
+        }
     }
 }
