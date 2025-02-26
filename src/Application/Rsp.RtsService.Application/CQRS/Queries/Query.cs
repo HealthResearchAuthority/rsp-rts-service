1afc8165b5f423fa7f4626c5df41aa4d59ef43f5
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-02-26 16:49:40 +0000

    - remove unused file references in projects

--- src/Application/Rsp.RtsService.Application/CQRS/Queries/Query.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using Rsp.RtsService.Application.DTOS.Responses;
-
-namespace Rsp.RtsService.Application.CQRS.Queries;
-
-public class Query(int id) : IRequest<QueryResponse>
-{
-    public int Id { get; } = id;
-}
