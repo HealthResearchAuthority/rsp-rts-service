333398f11d740c56c3794be744d2c8ac0c889a89
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-04-22 09:18:52 +0100

    RSP-3201 Add Audit History (#12)
    * RSP-3201 Add Audit History
    
    * Added Ilogger Implementation to Audit Service

---
+++ src/Domain/Rsp.RtsService.Domain/Entities/Audit.cs
@@ -0,0 +1,8 @@
+﻿namespace Rsp.RtsService.Domain.Entities;
+
+public class Audit
+{
+    public int Id { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string Description { get; set; } = null!;
+}
